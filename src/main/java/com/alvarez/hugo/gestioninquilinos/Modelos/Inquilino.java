77bf114f464d34e8e359b1881a1bc83cf47cc96d
Nelkha <maritogallego@hotmail.com>
2023-03-15 10:49:37 -0300

    creacion de modelo inquilino

---
+++ src/main/java/com/alvarez/hugo/gestioninquilinos/Modelos/Inquilino.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.alvarez.hugo.gestioninquilinos.Modelos;
+
+/**
+ *
+ * @author Mario
+ */
+public class Inquilino {
+    private String nombre;
+    private String apellido;
+    private String cuit;
+    private String telefono;
+    
+    
+}
